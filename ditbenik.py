95f093c3a77d9d9e2cee2f2f5ae2c4ca19b2b325
TristanTm <33825@ma-web.nl>
2021-09-22 09:42:41 +0200

    ditbenik_opdarcht_BO

---
+++ ditbenik.py
@@ -0,0 +1,19 @@
+print("Hello You!, ik ben Tristan ")
+
+Name = input("Wie ben jij? ")
+print("Hello " + Name ) 
+
+
+
+
+Vraag = input("Waar woon ik in Noord Holland: A) Zaandam. B) Alkmaar. C) Den Helder. [A/B/C]? : " )
+
+if Vraag == "A" :
+    print("Yes dat klopt! Ik woon nu al 14 jaar in Zaandam")
+elif Vraag == "B" : 
+    print("Nee ik woon niet in Alkmaar, ik ben er wel een paar keer geweest ")
+elif Vraag == "C" : 
+    print("Ik woon niet in Den Helder, ik kom er wel vaak als ik naar texel ga!")
+
+import time
+time.sleep(30)
