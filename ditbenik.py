5542f74a9028dd386295928004888e29ef143f85
TristanTm <33825@ma-web.nl>
2021-09-22 09:53:16 +0200

    ditbenik

--- ditbenik.py
+++ ditbenik.py
@@ -9,10 +9,29 @@ print("Hello " + Name )
 Vraag = input("Waar woon ik in Noord Holland: A) Zaandam. B) Alkmaar. C) Den Helder. [A/B/C]? : " )
 
 if Vraag == "A" :
-    print("Yes dat klopt! Ik woon nu al 14 jaar in Zaandam")
+    print("Yes dat klopt! Ik woon nu al 14 jaar in Zaandam ")
 elif Vraag == "B" : 
     print("Nee ik woon niet in Alkmaar, ik ben er wel een paar keer geweest ")
 elif Vraag == "C" : 
     print("Ik woon niet in Den Helder, ik kom er wel vaak als ik naar texel ga!")
 
 
+
+Vraag2 = input("Welke games speel ik het meest: A) Overwatch. B) Apex Legends. C) GTA 5. [A/B/C]? :")
+
+if Vraag2 == "A" :
+    print("Helaas niet, ik speel het wel veel maar ik speel het niet het meest ")
+elif Vraag2 == "B" :
+    print("Jup! Ik speel APex legends als sinds de dag dat het uit kwam! ")
+elif Vraag2 == "C" :
+    print("Ik speel het de laatste tijd wel meer maar het is niet wat ik het meeste speel ")
+
+
+Vraag3 = input("Welke huisdieren heb ik: A) Hond en Kat. B) Vogels en Kat. C) Alleen een kat. [A/B/C]? :")
+
+if Vraag3 == "A" : 
+    print("Nope ik heb er hier maar een van de twee, ik zou wel graag een hond willen ")
+elif Vraag3 == "B" :
+    print("Jup! Ik heb zelf 2 vogels en we hebben dan ook nog een kat! Niet de beste combo haha ")
+elif Vraag3 == "C" : 
+    print("Ik heb wel een hele lieve kat maar daarnaast ook nog andere dieren dus niet alleen een kat ")
