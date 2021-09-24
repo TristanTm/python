d8aea7fa2cd62f86886e0881d5cf3bef34c999cc
TristanTm <33825@ma-web.nl>
2021-09-24 09:29:51 +0200

    PoemPrinting

---
+++ PYTB1L2PoemPrinting/Poem Printing.py
@@ -0,0 +1,8 @@
+vers1 = "Ik ben Tristan Timmerman"
+vers2 = "Ik speel golf en games wanneer ik kan"
+vers3 = "Ik ga vaak naar het buitenland op vakantie"
+vers4 = "En dat ik op Tijd kom is een garantie"
+vers5 = "Ik spreek vaak af met vrienden"
+vers6 = "Die ik allemaal lang geleden bevriende."
+
+print(vers1,vers2,vers3,vers4,vers5,vers6)
