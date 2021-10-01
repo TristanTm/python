eab18f341611d0219e0fefa75140ff4f69c7cea2
TristanTm <33825@ma-web.nl>
2021-10-01 09:54:11 +0200

    Daily Choces

---
+++ PYTB1L3DailyChoices/Daily Choices.py
@@ -0,0 +1,46 @@
+print("Je wekker gaat af, slaap je verder of sta je op?")
+choice = input()
+if choice == 'Opstaan':
+    print("Je staat op en maakt je klaar voor de dag")
+elif choice == 'Slapen':
+    print("Slaap lekker")
+else:
+    print(choice, " Dat was niet een keuze")
+
+print("Je bent wakker, ga je douchen of ontbijten?")
+choice = input()
+if choice == 'Douchen':
+    print("Je gaat lekker warm douchen")
+elif choice == 'Ontbijten':
+    print("Je maakt en broodje kaas en eet het op.")
+else:
+    print(choice, " Dat was niet een keuze")
+
+print("Je pizza is aangebrand in de oven, maak je een nieuwe of eet je hem alsnog op?")
+choice = input()
+if choice == 'Opeten':
+    print("Je eet de pizza en krijgt buikpijn.")
+elif choice == 'Nieuwe Maken':
+    print("Je maakt nog een pizza en laat deze niet verbranden. Goed werk. ")
+else:
+    print(choice, " Dat was niet een keuze")
+
+
+print("Je valt van een gebouw af. Pak je je paraplu of ga je dood?")
+choice = input()
+if choice == 'Dood':
+    print("Je valt te pletter maar je bent built different dus je overleeft het.")
+elif choice == 'Paraplu':
+    print("Je pakt je paraplu erbij en land veilig op de weg.")
+else:
+    print(choice, " Dat was niet een keuze")
+
+
+print("Je gaat naar de supermarkt, koop je spullen voor nasi of voor pizza")
+choice = input()
+if choice == 'Nasi':
+    print("Je maakt een nasi en je eet het op.")
+elif choice == 'Pizza':
+    print("Je maakt een pizza en laat het aanbranden in de oven.")
+else:
+    print(choice, " Dat was niet een keuze")
