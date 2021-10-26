7d0f291ea2b5ea29912fb757b9074282fdf09761
TristanTm <33825@ma-web.nl>
2021-10-26 12:50:16 +0200

    Les over Lists en Floats

---
+++ Lists-en-Floats/floats.py
@@ -0,0 +1,19 @@
+list = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8]
+list.append(3.2)
+
+my_var1 = list.pop(0)
+my_var2 = list.pop(0)
+som2 = my_var1 + my_var2
+print(som2)
+
+print(list)
+
+som = list[0] + list[1] + list[2]
+print(som)
+print(list[0+1+2])
+
+for x in list:
+    print(x)
+
+
+
